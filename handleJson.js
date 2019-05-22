6ea9cd97f4962033c7ef9f845cfdc7830991b25f
hyperminer <huancnttuet@gmail.com>
2019-05-22 11:16:29 +0700

    sort by age

--- handleJson.js
+++ handleJson.js
@@ -1,5 +1,8 @@
 var mydata = JSON.parse(data);
 var string = `<table><tr><th>name</th><th>Age</th></tr>`
+mydata.sort(function (a, b) {
+  return(a.age - b.age)
+})
 mydata.map((value, index) => {
    string += `<tr><td>${value.name}</td><td>${value.age}</td></tr>`
 
