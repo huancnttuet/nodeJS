adef4f0047dc051c2296f7b2f34662ccde9e4be9
hyperminer <huancnttuet@gmail.com>
2019-06-03 16:03:10 +0700

    2 table

--- handleJson.js
+++ handleJson.js
@@ -8,4 +8,20 @@ mydata.map((value, index) => {
 
 })
 string+=`</table>`
-document.getElementById("tb").innerHTML= string
+
+document.getElementById("tb1").innerHTML= string
+document.getElementById("tb2").innerHTML= `<table><tr><th>name</th><th>Age</th></tr></table>`
+document.getElementById("tb1").style.display = "none";
+document.getElementById("tb2").style.display = "none";
+
+function myFunction() {
+  var tb1 = document.getElementById("tb1");
+  var tb2 = document.getElementById("tb2");
+  if (tb1.style.display === "none") {
+    tb1.style.display = "block";
+    tb2.style.display = "none"
+  } else {
+    tb1.style.display = "none";
+    tb2.style.display = "block";
+  }
+}
